6bd37cbcc3d0e7d47bccda30a0f489fd063de866
David Lee <dav@davlee.com>
2025-08-14 11:30:34 +1000

    fix: add Warn to logger iface

--- pkg/commandment/operation.go
+++ pkg/commandment/operation.go
@@ -71,6 +71,7 @@ func mustMarshal(v interface{}) json.RawMessage {
 type Logger interface {
 	Info(msg string, keysAndValues ...interface{})
 	Error(msg string, keysAndValues ...interface{})
+	Warn(msg string, keysAndValues ...interface{})
 	Debug(msg string, keysAndValues ...interface{})
 }
 
